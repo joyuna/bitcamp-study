178f102c726935137169b7f21babe73ab2724cff
joyuna <joayounga@gmail.com>
2022-08-01 08:31:51 +0900

    22.07.29

--- board-app/app/src/main/java/com/bitcamp/board/dao/MemberList.java
+++ board-app/app/src/main/java/com/bitcamp/board/dao/MemberList.java
@@ -11,7 +11,7 @@ public class MemberList extends ObjectList {
   // 서브 클래스의 get() 메서드에서 처리할 상황이 아니다.
   // 서브 클래스의 get()을 호출한 쪽에 보고하는 것이 더 낫다.
   // 이럴경우 try ~ catch ~ 를 쓰지 말고 메서드 선언부에 발생되는 예외를 표시하라!
-  public Member get(String email) throws Throwable {
+  public Member get(String email) {
     for (int i = 0; i < size(); i++) {
       Member member = (Member) get(i);
       if (member.email.equals(email)) {
@@ -25,7 +25,7 @@ public class MemberList extends ObjectList {
   // 서브 클래스의 remove() 메서드에서 처리할 상황이 아니다.
   // 서브 클래스의 remove()를 호출한 쪽에 보고하는 것이 더 낫다.
   // 이럴경우 try ~ catch ~ 를 쓰지 말고 메서드 선언부에 발생되는 예외를 표시하라!
-  public boolean remove(String email) throws Throwable {
+  public boolean remove(String email) {
     for (int i = 0; i < size(); i++) {
       Member member = (Member) get(i);
       if (member.email.equals(email)) {
