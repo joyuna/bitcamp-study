5438da87698c397678f00bcd00fbdd982611e773
joyuna <joayounga@gmail.com>
2022-08-19 09:24:56 +0900

    22.08.18

--- java-lang/app/src/main/java/com/eomcs/basic/ex07/Exam0340.java
+++ java-lang/app/src/main/java/com/eomcs/basic/ex07/Exam0340.java
@@ -26,17 +26,21 @@ public class Exam0340 {
       return Objects.hash(age, name);
     }
 
-    @Override
-    public boolean equals(Object obj) {
-      if (this == obj)
-        return true;
-      if (obj == null)
-        return false;
-      if (getClass() != obj.getClass())
-        return false;
-      Member other = (Member) obj;
-      return age == other.age && Objects.equals(name, other.name);
-    }
+
+    //    @Override
+    //    public boolean equals(Object obj) {
+    //      if (this == obj)
+    //        return true;
+    //      if (obj == null)
+    //        return false;
+    //      if (getClass() != obj.getClass())
+    //        return false;
+    //      Member other = (Member) obj;
+    //      return age == other.age && Objects.equals(name, other.name);
+    //    }
+    //
+
+
   }
 
   public static void main(String[] args) {
