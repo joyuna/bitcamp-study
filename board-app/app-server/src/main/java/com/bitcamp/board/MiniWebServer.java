4a00d3b7da48f3ac01c86217fe65f737b5562d99
joyuna <joayounga@gmail.com>
2022-09-16 02:05:18 +0900

    0915ver52.미니웹서버스텝6 정말 완료

--- board-app/app-server/src/main/java/com/bitcamp/board/MiniWebServer.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/MiniWebServer.java
@@ -20,8 +20,15 @@ import com.bitcamp.board.handler.WelcomeHandler;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer; 
+import com.sun.net.httpserver.HttpServer;
 
+// 1) 기본 웹 서버 만들기
+// 2) 한글 콘텐트를 출력하기
+// 3) HTML 콘텐트를 출력하기
+// 4) 메인 화면을 출력하는 요청처리 객체를 분리하기
+// 5) 요청 자원의 경로를 구분하여 처리하기
+// 6) 게시글 요청 처리하기
+//
 public class MiniWebServer {
 
   public static void main(String[] args) throws Exception {
@@ -35,21 +42,16 @@ public class MiniWebServer {
     ErrorHandler errorHandler = new ErrorHandler();
     BoardHandler boardHandler = new BoardHandler(boardDao);
 
-    // 클라이언트 요청을 처리하는 객체
     class MyHttpHandler implements HttpHandler {
       @Override
       public void handle(HttpExchange exchange) throws IOException {
-
-        // 1-4) 클라이언트 요청이 들어왔을 때 마다 호출된다.
         System.out.println("클라이언트가 요청함!");
-
 
         URI requestUri = exchange.getRequestURI();
         String path = requestUri.getPath();
         String query = requestUri.getQuery();
         byte[] bytes = null;
 
-        //1-5) 응답할 콘텐트 준비
         try (StringWriter stringWriter = new StringWriter();
             PrintWriter printWriter = new PrintWriter(stringWriter)) {
 
@@ -61,34 +63,37 @@ public class MiniWebServer {
               paramMap.put(kv[0], kv[1]);
             }
           }
-
           System.out.println(paramMap);
 
           if (path.equals("/")) {
             welcomeHandler.service(paramMap, printWriter);
+
           } else if (path.equals("/board/list")) {
-            boardHandler.list(paramMap,  printWriter);
+            boardHandler.list(paramMap, printWriter);
+
           } else if (path.equals("/board/detail")) {
-            boardHandler.detail(paramMap,  printWriter);
+            boardHandler.detail(paramMap, printWriter);
+
           } else if (path.equals("/board/update")) {
-            boardHandler.update(paramMap,  printWriter);
+            boardHandler.update(paramMap, printWriter);
+
           } else if (path.equals("/board/delete")) {
-            boardHandler.delete(paramMap,  printWriter);
-          } else if (path.equals("/board/list")) {
-            boardHandler.delete(paramMap,  printWriter);
+            boardHandler.delete(paramMap, printWriter);
+
           } else {
-            errorHandler.error(paramMap,  printWriter);
+            errorHandler.error(paramMap, printWriter);
           }
 
-          bytes = stringWriter.toString().getBytes("UTF-8"); 
+          bytes = stringWriter.toString().getBytes("UTF-8");
+
         } catch (Exception e) {
-          bytes = "요청 처리 중 오류 발생!!".getBytes("UTF-8"); 
-          e.printStackTrace(); 
+          bytes = "요청 처리 중 오류 발생!".getBytes("UTF-8");
+          e.printStackTrace(); // 서버 콘솔 창에 오류에 대한 자세한 내용을 출력한다.
         }
 
         // 1-9) 보내는 콘텐트의 MIME 타입이 무엇인지 응답 헤더에 추가한다.
         Headers responseHeaders =exchange.getResponseHeaders();
-        responseHeaders.add("Content-Type", "text/plain; charset=UTF-8");
+        responseHeaders.add("Content-Type", "text/html; charset=UTF-8");
         // 1-6) 응답헤더 전송 
         exchange.sendResponseHeaders(200, bytes.length);
         // 1-7) 콘텐트 출력 스트림 준비
