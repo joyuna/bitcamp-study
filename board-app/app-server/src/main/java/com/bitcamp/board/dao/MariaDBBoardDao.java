61cbc48d5368a9bc0604a74f2647c7a76a391f58
joyuna <joayounga@gmail.com>
2022-09-21 12:57:25 +0900

    0921 레파지토리 정정

--- board-app/app-server/src/main/java/com/bitcamp/board/dao/MariaDBBoardDao.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/dao/MariaDBBoardDao.java
@@ -6,9 +6,7 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 import com.bitcamp.board.domain.Board;
-import com.bitcamp.servlet.annotation.Repository;
 
-@Repository("boardDao") //톰캣서버는 우리가 만든 레파지토리는 모른다.
 public class MariaDBBoardDao implements BoardDao {
 
   Connection con;
