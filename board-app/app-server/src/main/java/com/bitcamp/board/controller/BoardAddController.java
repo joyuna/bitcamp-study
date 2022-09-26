92bb9bee2c8f47364d357ec678700d59a2d18fcb
joyuna <joayounga@gmail.com>
2022-09-26 12:43:05 +0900

    220926 ver.62 1-9 web.xml 오류 정정

--- board-app/app-server/src/main/java/com/bitcamp/board/controller/BoardAddController.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/controller/BoardAddController.java
@@ -1,7 +1,6 @@
 package com.bitcamp.board.controller;
 
 import java.io.IOException;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -36,23 +35,13 @@ public class BoardAddController extends HttpServlet {
         throw new Exception("게시글 등록 실패!");
       }
 
-      // JSP에게 UI 생성을 위임한다.
-      resp.setContentType("text/html;charset=UTF-8"); //  JSP가 출력할 콘텐트의 MiMe 타입 설
-      RequestDispatcher 요청배달자 = req.getRequestDispatcher("/board/list.jsp");
-      요청배달자.include(req, resp); // JSP 실행후 리턴된다.
+
+      response.setContentType("text/html;charset=UTF-8"); 
+      request.getRequestDispatcher("/board/add.jsp").include(request, response);
 
     } catch (Exception e) {
-
-      // 예외가 발생하면 예외를 처리하는 JSP에게 UI 생성을 위임한다.
-      RequestDispatcher 요청배달자 = req.getRequestDispatcher("/error.jsp");
-
-      // JSP를 실행하기 전에 ServletRequest 보관소에 오류 정보를 담는다.
-      req.setAttribute("exception", e);
-
-      // forward():
-      // - 예외가 발생하면 기존의 출력 내용을 모두 버린다.
-      // -JSP에게 처음부터 새로 출력하게 전권을 위임한다.
-      요청배달자.forward(req, resp); // JSP를 실행한 후 리턴된다.
+      request.setAttribute("exception", e);
+      request.getRequestDispatcher("/error.jsp").forward(request, response); 
     } 
   }
 }
