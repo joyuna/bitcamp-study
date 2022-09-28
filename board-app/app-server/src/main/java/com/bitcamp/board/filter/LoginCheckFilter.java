93c7d0f0642b6cca7ffd09bf2abacf6f3248beec
joyuna <joayounga@gmail.com>
2022-09-28 15:59:38 +0900

    220928 ver.67 1-2

--- board-app/app-server/src/main/java/com/bitcamp/board/filter/LoginCheckFilter.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/filter/LoginCheckFilter.java
@@ -6,13 +6,19 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 public class LoginCheckFilter implements Filter{
 
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
       throws IOException, ServletException {
-    // TODO Auto-generated method stub
+
+    // 필터의 파라미터로 넘어오는 객체는 원래 HttpServletRequest 객체이다.
+    // 세션처럼 HTTP 프로토콜과 관련된 기능을 쓰고 싶다면,
+    // 원래 타입으로 형변환 한 다음에 사용하라!
+    HttpServletRequest httpRequest = (HttpServletRequest) request;
+    request.getSession()
 
   }
 
