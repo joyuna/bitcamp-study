0348b7f3f77e708935249d71ffaac8025c4425c0
joyuna <joayounga@gmail.com>
2022-10-07 11:21:01 +0900

    221007 ver77

---
+++ board-app/app-server/src/main/java/com/bitcamp/servlet/DispatcherServlet.java
@@ -0,0 +1,28 @@
+package com.bitcamp.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// 역할: 
+// 페이지 컨트롤러의 앞쪽에서 클라이언트 요청을 받는 일을 한다.
+// 클라이언트가 요청한 경로에 따라 적절한 페이지 컨트롤러를 실행한다.
+// 페이지 컨트롤러의 공통 기능을 수행한다.
+//
+//@WebServlet(value = "/service/*")
+public class DispatcherServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  public void service(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+
+    // "/service" 다음에 오는 경로, 즉 *(와일드카드) 에 해당하는 경로를 리턴한다.
+    String pathInfo = req.getPathInfo();
+    System.out.println(pathInfo); 
+
+  }
+
+}
