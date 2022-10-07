13ab992d725434f8b22f79eb0ef6c9fb7453edb9
joyuna <joayounga@gmail.com>
2022-10-07 16:12:03 +0900

    221007 ver78

---
+++ board-app/app-server/src/main/java/com/bitcamp/servlet/Controller.java
@@ -0,0 +1,8 @@
+package com.bitcamp.servlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Controller {
+  String execute(HttpServletRequest request, HttpServletResponse response) throws Exception;
+}
