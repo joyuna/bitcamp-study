779f3495d927085a02a98e68e8ffbaea6bc18982
joyuna <joayounga@gmail.com>
2022-10-14 10:24:50 +0900

    22.10.14 프론트 컨트롤러의 경로를 /service -> /app 변경

--- board-app/app-server/src/main/java/com/bitcamp/board/listener/ContextLoaderListener.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/listener/ContextLoaderListener.java
@@ -36,33 +36,34 @@ public class ContextLoaderListener implements ServletContextListener {
 
       // 자바 코드로 서블릿 객체를 직접 생성하여 서버에 등록하기
       DispatcherServlet servlet = new DispatcherServlet(iocContainer);
-      Dynamic config = ctx.addServlet("DispatcherServlet", servlet);
-      config.addMapping("/service/*");
+      Dynamic config = ctx.addServlet("app", servlet);
+      config.addMapping("/app/*");
       config.setMultipartConfig(new MultipartConfigElement(
           this.getClass().getAnnotation(MultipartConfig.class)));
       config.setLoadOnStartup(1); // 웹 애플리케이션을 시작할 대 프론트 컨트롤러를 자동 생성한다.
 
-      // 필터 등록
+      // "app" 이름의 프론트 컨트롤러에 필터를 붙인다.
       CharacterEncodingFilter filter = new CharacterEncodingFilter("UTF-8");
       FilterRegistration.Dynamic filterConfig = ctx.addFilter("CharacterEncodingFilter", filter);
       filterConfig.addMappingForServletNames(
           EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.INCLUDE),  // EumSet 상수들의 배열 
           false, 
-          "DispatcherServlet");
+          "app");
 
+      // 특정 URL에 필터를 붙인다.
       AdminCheckFilter adminFilter = new AdminCheckFilter();
       FilterRegistration.Dynamic adminFilterConfig = ctx.addFilter("AdminCheckFilter", adminFilter);
       adminFilterConfig.addMappingForUrlPatterns(
           EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.INCLUDE),  // EumSet 상수들의 배열 
           false, 
-          "/sevice/member/*");
+          "/app/member/*");
 
       LoginCheckFilter loginFilter = new LoginCheckFilter();
       FilterRegistration.Dynamic loginFilterConfig = ctx.addFilter("LoginCheckFilter", loginFilter);
       loginFilterConfig.addMappingForUrlPatterns(
           EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.INCLUDE),  // EumSet 상수들의 배열 
           false, 
-          "/sevice/*");
+          "/app/*");
 
     } catch (Exception e) {
       e.printStackTrace();
