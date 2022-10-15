1a46658b45141b932d5711418516cf6ad5580890
joyuna <joayounga@gmail.com>
2022-10-15 13:04:12 +0900

    22.10.14 WebApplicationInitializer구현 1-4 AbstractAnnotationConfigDispatcherServletInitializer.

--- board-app/app-server/src/main/java/com/bitcamp/board/conifg/AppWebApplicationInitializer.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/conifg/AppWebApplicationInitializer.java
@@ -3,47 +3,40 @@ package com.bitcamp.board.conifg;
 import javax.servlet.Filter;
 import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletRegistration.Dynamic;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 import com.bitcamp.board.filter.AdminCheckFilter;
 import com.bitcamp.board.filter.LoginCheckFilter;
 
 // 서블릿 컨테이너에서 웹 애플리케이션을 시작할 때 :
 // 수퍼 클래스를 분석하시오!! 제 ~ 발!!
-// ===> SpringServletContainerInitializer.onStartup() 호출
-//   ===> AppWebApplicationInitializer.onStartup() 호출
-//     ===> AbstractContextLoaderInitializer.onStartup() 호출
-//       ===> registerContextLoaderListener() 호출
-//         ===> createRootApplicationContext() 호출
-//       ===> IoC 컨테이너, 프론트 컨트롤러, 필터 준비
+//
 
-// @MultipartConfig(maxFileSize = 1024 * 1024 * 10) 
-public class AppWebApplicationInitializer extends AbstractDispatcherServletInitializer {
+public class AppWebApplicationInitializer 
+extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-  // 수퍼클래스에서 ContextLoaderListener를 준비할 때 사용할 Root IoC 컨테이너를 리턴한다.
+  // 수퍼클래스에서 Root IoC 컨테이너를 만들어 준단다.
+  // 그럼 우리가 해야 할 일은 그 컨테이너가 사용할 클래스 정보만 알려주면 된다.
   @Override
-  protected WebApplicationContext createRootApplicationContext() {
-    return null; // 설정할 필요가 없다면 null을 리턴
+  protected Class<?>[] getRootConfigClasses() {
+    return null;
   }
 
+  // 수퍼클래스에서 DispatcherServlet 을 준비할 때 사용할 서블릿 이름을 리턴한다.
   @Override
   protected String getServletName() {
     return "app";
   }
 
-  // 수퍼클래스에서 DispatcherServlet을 준비할 때 사용할 IoC 컨테이너를 등록한다.
+  // 수퍼클래스에서 DispatcherServlet 이 사용할 IoC 컨테이너를 만들어 준단다.
+  // 그럼 우리가 할 일은 그 컨테이너를 만들 때 사용할 java config 클래스를 알려주면 된다.
   @Override
-  protected WebApplicationContext createServletApplicationContext() {
-    // 웹관련 컴포넌트를 다룰 수 있는 기능이 포함된 스프링 IoC 컨테이너 준비
-    AnnotationConfigWebApplicationContext iocContainer =
-        new AnnotationConfigWebApplicationContext();
-    iocContainer.register(AppConfig.class);
-    return iocContainer;
+  protected Class<?>[] getServletConfigClasses() {
+    // java config 클래스 정보를 배열에 담아서 리턴한다.
+    return new Class<?>[] {AppConfig.class};
   }
 
-  // 수퍼클래스에서 DispatcherServlet의 URL을 연결할 때 사용할 경로를 리턴한다.
+  // 수퍼클래스에서 DispatcherServlet 의 URL을 연결할 때 사용할 경로를 리턴한다.
   @Override
   protected String[] getServletMappings() {
     return new String[] {"/app/*"}; // URL이 하나여도 배열로 리턴해야한다.
@@ -52,14 +45,12 @@ public class AppWebApplicationInitializer extends AbstractDispatcherServletIniti
   // 수퍼클래스에서 필터를 등록할 때 사용할 정보를 리턴한다.
   @Override
   protected Filter[] getServletFilters() {
-
     return new Filter[] {
         new CharacterEncodingFilter("UTF-8"),
         new AdminCheckFilter(),
         new LoginCheckFilter()
     };
   }
-
   //  // "app" 이름의 프론트 컨트롤러에 필터를 붙인다.
   //  CharacterEncodingFilter filter = ;
   //  FilterRegistration.Dynamic filterConfig = servletContext.addFilter("CharacterEncodingFilter", filter);
@@ -84,16 +75,12 @@ public class AppWebApplicationInitializer extends AbstractDispatcherServletIniti
   //      "/app/*"); // ?! URL로 경로를 설정하면 경로 정교하게 설정할 수 있지만 필터를 붙이면 정교하게 설정할 수 없다. 퉁치는 느낌 .. 
 
 
-  // 수퍼클래스에서 DispatcherServlet을 준비할 때 추가적으로 설정할 것이 있으면 설정한다.
+  // 수퍼클래스에서 DispatcherServlet 을 준비할 때 추가적으로 설정할 것이 있으면 설정한다.
   @Override
   protected void customizeRegistration(Dynamic registration) {
-    // 1) 멀티파트 설정 정보를 애노테이션에서 가져오기
-    // config.setMultipartConfig(new MultipartConfigElement(
-    //      this.getClass().getAnnotation(MultipartConfig.class)));
-    // 2) 멀티파트 설정 정보를 직접 지정하기
     registration.setMultipartConfig(new MultipartConfigElement(
-        System.getProperty("java.io.tmpdir"), // 클라이언트가 보낸 파일을 임시 저장할 디렉토리
-        1024 * 1024 * 5,  // 한 개 파일의 최대 크기
+        System.getProperty("java.io.tmpdir"), // 클라이언트가 보낸 파일을 임시 저장할 때 사용할 디렉토리
+        1024 * 1024 * 5, // 한 파일의 최대 크기
         1024 * 1024 * 10, // 첨부 파일을 포함한 전체 요청 데이터의 최대 크기
         1024 * 1024 // 첨부 파일 데이터를 일시적으로 보관할 버퍼 크기 
         ));
