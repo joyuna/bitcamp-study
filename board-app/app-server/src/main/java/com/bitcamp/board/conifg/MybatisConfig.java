550a2822e6c1d5e3b375ba1af9535ea830399fe9
joyuna <joayounga@gmail.com>
2022-10-17 14:48:27 +0900

    22.10.17 ver85. Mybatis설정 3 SqlSessionFactory 객체준비

--- board-app/app-server/src/main/java/com/bitcamp/board/conifg/MybatisConfig.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/conifg/MybatisConfig.java
@@ -1,11 +1,10 @@
 package com.bitcamp.board.conifg;
 
-import java.io.InputStream;
 import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 // DatabaseConfig와 DataSource만 설정
 public class MybatisConfig {
@@ -15,14 +14,25 @@ public class MybatisConfig {
   }
 
   @Bean
-  public SqlSessionFactory sqlSessionFactory(DataSource ds) {
-    System.out.println("PlatformTransactionManager 객체 생성!");
-    String resource = "com/bitcamp/board/config/mybatis-config.xml";
-    InputStream inputStream = Resources.getResourceAsStream(resource);
-    SqlSessionFactory sqlSessionFactory =
-        new SqlSessionFactoryBuilder().build(inputStream);
+  public SqlSessionFactory sqlSessionFactory(
+      DataSource ds,
+      ApplicationContext iocContainer
+      ) throws Exception {
 
-    return new DataSourceTransactionManager(ds);
+    // SqlSessionFactory를 만들어 줄 객체를 준비한다.
+    SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+
+    // Mybatis가 사용할 DB 커넥션풀을 설정한다.
+    factoryBean.setDataSource(ds);
+
+    // Mybatis가 실행할 SQL 문이 들어 있는 파일의 위치를 설정한다.
+    //  Resource mapperLocation = iocContainer.getResource("com/bitcamp/board/mapper/*Mapper.xml"); => 쓸데없는 객체를 만들지 않기 위해 아래와 같이 줄임.
+    //    factoryBean.setMapperLocations(mapperLocation);
+    factoryBean.setMapperLocations(
+        iocContainer.getResource("classpath:com/bitcamp/board/mapper/*Mapper.xml")); // classpath: 접두어를 붙여주는 경우도 있고, 붙여도 된다.
+
+    return factoryBean.getObject();
   }
 }
+
 
