08273be509498baa796cc9d935532fc0d28822bb
joyounga <joayounga@gmail.com>
2023-01-03 23:42:47 +0900

    javascript & css

---
+++ eomcs-web/app/src/main/java/com/eomcs/web/javascript/Ex07Controller.java
@@ -0,0 +1,12 @@
+package com.eomcs.web.javascript;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Ex07Controller {
+
+  @RequestMapping("/javascript/ex07/test1")
+  public void test1() throws Exception {
+  }
+}
